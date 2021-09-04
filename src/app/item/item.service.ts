cf5c1dc28f0cac119208138ff8a1431ebfda1f8f
lesmacar31 <lcastillor4@miumg.edu.gt>
2021-09-03 19:20:48 -0600

    Agregado descripcion

--- src/app/item/item.service.ts
+++ src/app/item/item.service.ts
@@ -7,11 +7,11 @@ import { Item } from './item'
 })
 export class ItemService {
   private items = new Array<Item>(
-    { id: 1, name: 'Lesly Castillo', role: 'Reina' },
-    { id: 2, name: 'Karol Castillo', role: 'Cocinera' },
-    { id: 3, name: 'Esther Castillo', role: 'Princesa' },
-    { id: 4, name: 'Victor Castillo', role: 'Caballero' },
-    { id: 5, name: 'Carlitos Castillo', role: 'Obrero' }
+    { id: 1, name: 'Lesly Castillo', descripcion: 'Unica e imcomparable y esponjosa.', role: 'Reina' },
+    { id: 2, name: 'Karol Castillo', descripcion: 'Comida los tres tiempos', role: 'Cocinera' },
+    { id: 3, name: 'Esther Castillo', descripcion: 'Se parece a la reina', role: 'Princesa' },
+    { id: 4, name: 'Victor Castillo', descripcion: 'Es un buen sujueto', role: 'Caballero' },
+    { id: 5, name: 'Carlitos Castillo', descripcion: 'A veces trabaja', role: 'Obrero' }
   )
 
   getItems(): Array<Item> {
